fc19565d0cc29ed64e5c2bfc921b79b17ffec020
MrAdityaAlok <mradityaalok21@gmail.com>
2020-11-06 00:51:11 +0530

    install script updated

--- install.sh
+++ install.sh
@@ -6,60 +6,98 @@ alias ee='echo -e'
 
 #Greetings
 echo
-ee "\e[93mThis script will install openjdk-11.0.1 in Termux."
+ee "\e[93mThis script will try to install openjdk-11.0.1 in Termux."
 ee "\e[93mLibraries compiled and  scripted by \e[32mMrAdityaAlok."
 echo
 
 #Checking for existing Java installation
-if [ -e $PREFIX/bin/java ]
+if [ -e "$PREFIX"/bin/java ]
 then
 	ee "\e[32mJava is already installed!"
 	echo
 	exit
 else
-	#Checking, whether is someone trying to cheat and simplyfy their installation it on Linux (i.e. x86 (not listad, as you can see) machine) using this script, which have no reason to work.
-	case `dpkg --print-architecture` in
+	#Checking architecture
+	case $(dpkg --print-architecture) in
 	aarch64)
-		archname="aarch64"; tag="v8" ;;
+		archname="aarch64" ;;
 	arm64)
-		archname="aarch64"; tag="v8" ;;
+		archname="aarch64";;
 	armhf)
-		archname="arm"; tag="v8-151" ;;
+		archname="arm" ;;
 	armv7l)
-		archname="arm"; tag="v8-151" ;;
+		archname="arm" ;;
 	arm)
-		archname="arm"; tag="v8-151" ;;
+		archname="arm" ;;
 	*)
 		ee "\e[91mERROR: Unknown architecture."; echo; exit ;;
 	esac
 	
 	#Actual installation
-	ee "\e[32m[Downloading...] \e[34mopenjdk-11.0.1 (248MB) for ${archname}..."
-	ee "\e[32m[Downloading...] Pleae wait for some time...."
-	
+	ee "\e[32m[*] \e[34mopenjdk-11.0.1 (248MB) for ${archname}..."
+	ee "\e[32m[*] \e[34mPleae wait for some time....\e[0m"
+
 	pkg install wget -y
 	wget https://github.com/MrAdityaAlok/openjdk-in-termux/releases/download/v1.0.0/openjdk-11.0.1.tar.gz
+	wgetreturn=$?
+	if [[ $wgetreturn -eq 0 ]]
+	then
+    	ee "\e[32m[*] \e[34mMoving JDK to system..."
+    	mv openjdk-11.0.1.tar.gz "$PREFIX"/share
 	
-	ee "\e[32m[*] \e[34mMoving JDK to system..."
-	mv openjdk-11.0.1.tar.gz $PREFIX/share
-	
-	ee "\e[32m[*] \e[34mExtracting JDK..."
-	cd $PREFIX/share
-	tar -xzvf openjdk-11.0.1.tar.gz
-	
-	ee "\e[32m[*] \e[34mSeting-up environment variable %JAVA_HOME%..."
-	export JAVA_HOME=$PREFIX/share/openjdk-11.0.1
-	echo "export JAVA_HOME=$PREFIX/share/openjdk-11.0.1/bin" >> $HOME/.profile
-	
-	
-	
-	ee "\e[32m[*] \e[34mCleaning up temporary files..."
-	rm -rf $HOME/install.sh
-	rm -rf $PREFIX/share/openjdk-11.0.1.tar.gz
-
-	
-	echo
-	ee "\e[32mJava was successfully installed!\e[39m"
-	echo "Enjoy ...."
-	echo
+    	ee "\e[32m[*] \e[34mExtracting JDK..."
+    	cd "$PREFIX"/share || exit
+    	pkg install tar -y
+    	tar -xzvf openjdk-11.0.1.tar.gz
+    	
+    	ee "\e[32m[*] \e[34mSeting-up environment variable %JAVA_HOME%..."
+    	export JAVA_HOME=$PREFIX/share/openjdk-11.0.1
+    	echo "export JAVA_HOME=$PREFIX/share/openjdk-11.0.1" >> "$HOME"/.profile
+    	PATH=$PATH:$PREFIX/share/openjdk-11.0.1/bin
+    	echo "PATH=$PATH:$PREFIX/share/openjdk-11.0.1/bin" >> "$HOME"/.profile
+    	cd "$HOME" || exit
+    	
+    	ee "\e[32m[*] \e[34mCleaning up temporary files..."
+    	rm -rf "$PREFIX"/share/openjdk-11.0.1.tar.gz
+    
+    	
+    	echo
+    	ee "\e[32mJava was successfully installed!\e[0m"
+    	java --version
+    	echo
+    else 
+    echo
+        ee "\e[31mwget exited with an error code : $wgetreturn"
+        case $wgetreturn in
+        1)
+            ee "\e[31mError : Generic code error\e[0m"
+            ;;
+        2)
+            ee "\e[31mError : Parse error-for instance, when parsing command-line options, the .wgetrc or .netrc...\e[0m"
+            ;;
+        3)
+            ee "\e[31mError : File I/O error.\e[0m"
+            ;;
+        4)
+            ee "\e[31mError : Network failure.\e[0m"
+            ;;
+        5)
+            ee "\e[31mError : SSL verification failure.\e[0m"
+            ;;
+        6)
+            ee "\e[31mError : Username/password authentication failure\e[0m"
+            ;;
+        7)
+            ee "\e[31mError : Protocol errors.\e[0m"
+            ;;
+        8) 
+            ee "\e[31mError : Server issued an error response.\e[0m"
+            ;;
+        esac
+        
+        rm -rf openjdk-11.0.1.tar.gz
+        echo
+        exit
+    fi
+        
 fi
